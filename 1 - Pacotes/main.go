e515835b6676785acadef4f6a5193f705039b348
Karoline Batista <karoline.batista@g.globo>
2025-09-09 15:16:44 -0300

    Importando e usando package externo com checkmail

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail" //importei um pacote externo
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("estudosgo@gmail.com") //chamando a função de um pacote externo
+	fmt.Println(erro)
 }
