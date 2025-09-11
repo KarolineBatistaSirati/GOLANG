10b197544d473c02fcdfd31525b02a50d3c678a5
Karoline Batista <karoline.batista@g.globo>
2025-09-11 14:49:41 -0300

    Exemplo de arrays internos adicionado

--- 8 - Arrays e Slices/arrays-e-slices.go
+++ 8 - Arrays e Slices/arrays-e-slices.go
@@ -33,4 +33,22 @@ func main() {
 	//esse slice é um pedaço do array2 e dentro do colchete põe os intervalos do índices que deseja
 	slice2 := array2[1:3]
 	fmt.Println(slice2)
+
+	//ARRAYS INTERNOS
+	slice3 := make([]float32, 10, 11) //tipo, tamanho(qnts itens tem) e capacidade(qnts consegue ter)
+	fmt.Println(slice3)
+	//saber tamanho e capacidade
+	fmt.Println(len(slice3)) //length tamanho
+	fmt.Println(cap(slice3)) //cap capacidade
+
+	slice3 = append(slice3, 5)
+	fmt.Println(slice3)
+
+	//quando ultrapassa a capacidade, ele dobra de tamanho e gera um novo slice
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
 }
--- 9 - Ponteiros/ponteiros.go
+++ 9 - Ponteiros/ponteiros.go
@@ -16,10 +16,11 @@ func main() {
 
 	//DIFERENÇA ENTRE ATRIBUIR VALOR ASSIM OU COM PONTEIROS?
 	var variavel3 int //o valor inicial é 0 e guarda, literalmente, o valor int
-	var ponteiro *int //o valor inicial é <NIL> e guardar o enreço de memória de um valor int
+	var ponteiro *int //o valor inicial é <NIL> e guardar o endereço de memória de um valor int
 
 	variavel3 = 100
 	ponteiro = &variavel3 //na frente da variável coloque um &
 
-	fmt.Println(variavel3, ponteiro)
+	fmt.Println(variavel3, ponteiro) //assim vai mostrar o endereço da memória, se quiser ver o valor colcoque um *na frente do ponteiro
+	//fmt.Println(variavel3, *ponteiro) //com * na frente mostra o conteúdo dentro da memória
 }
